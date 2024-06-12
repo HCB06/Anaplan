1b7413f7f1923a36398238c1331cd43fa35a3a6c
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-12 05:56:20 +0300

    Update plan(wine).py

--- Welcome to Pyerual Jetwork/ExampleCodes/plan(wine).py
+++ Welcome to Pyerual Jetwork/ExampleCodes/plan(wine).py
@@ -1,9 +1,9 @@
 import numpy as np
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import f1_score
-import plan
+import plan_di as pdi
 import time
 from colorama import Fore
 
@@ -15,31 +15,27 @@ one_hot_encoder = OneHotEncoder(sparse=False)
 y = one_hot_encoder.fit_transform(y.reshape(-1, 1))
 
 
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=100)
 
 
-scaler = StandardScaler()
-x_train = scaler.fit_transform(x_train)
-x_test = scaler.transform(x_test)
+x_train, x_test = pdi.standard_scaler(x_train, x_test)
 
 x_train = x_train.tolist()
 x_test = x_test.tolist()
 y_train = y_train.tolist()
 y_test = y_test.tolist()
 
-activation_potential = 0
-class_count = 3
 visualize = 'n'
 
-#x_train, y_train = plan.synthetic_augmentation(x_train, y_train, class_count)
-#x_test, y_test = plan.synthetic_augmentation(x_test, y_test, class_count)
+#x_train, y_train = pdi.synthetic_augmentation(x_train, y_train, class_count)
+#x_test, y_test = pdi.synthetic_augmentation(x_test, y_test, class_count)
 
-train_model = plan.fit(x_train, y_train, activation_potential)
-W = train_model[plan.get_weights()]
+train_model = pdi.fit(x_train, y_train)
+W = train_model[pdi.get_weights()]
 
-test_model = plan.evaluate(x_test, y_test, activation_potential, visualize, W)
-test_preds = test_model[plan.get_preds()]
-test_acc = test_model[plan.get_acc()]
+test_model = pdi.evaluate(x_test, y_test, visualize, W)
+test_preds = test_model[pdi.get_preds()]
+test_acc = test_model[pdi.get_acc()]
 
 y_test = np.array(y_test)
 
@@ -52,7 +48,7 @@ print('\nF1 Score:', f1)
 
 
 for i in range(len(x_test)):
-    Predict = plan.predict_model_ram(x_test[i], activation_potential, W)
+    Predict = pdi.predict_model_ram(x_test[i], W)
     time.sleep(0.6)
     if np.argmax(Predict) == y_test[i]:
         print(Fore.GREEN + 'Predicted Output(index):', np.argmax(Predict), 'Real Output(index):', y_test[i])
