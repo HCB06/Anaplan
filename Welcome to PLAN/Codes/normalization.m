79df1d85cdd0ae39861d2f97896642d1ec328407
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-12 06:13:17 +0300

    Delete Welcome to PLAN/Codes/normalization.m

--- Welcome to PLAN/Codes/normalization.m
+++
@@ -1,7 +0,0 @@
-function [normalized_vector] = normalization(vector)
-
-	abs_vector = abs(vector);
-    
-    max_abs_vector = max(abs_vector);
-    
-    normalized_vector = vector / max_abs_vector;
