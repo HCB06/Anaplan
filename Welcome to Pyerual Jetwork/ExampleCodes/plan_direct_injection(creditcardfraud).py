2c5e310c75f1d21d5cbfa76577d8fb9e0bc671ff
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-14 17:25:23 +0300

    Update and rename plan(creditcardfraud).py to plan_direct_injection(creditcardfraud).py

--- Welcome to Pyerual Jetwork/ExampleCodes/plan_direct_injection(creditcardfraud).py
+++ Welcome to Pyerual Jetwork/ExampleCodes/plan_direct_injection(creditcardfraud).py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
-import plan_bi as pbi
+import plan_di as pdi
 import numpy as np
 from sklearn.metrics import f1_score, confusion_matrix
 from colorama import Fore
@@ -26,22 +26,22 @@ y_test = encoder.transform(y_test.reshape(-1, 1))
 x_train = x_train.tolist()
 x_test = x_test.tolist()
 
-
+x_train, x_test = pdi.standard_scaler(x_train, x_test)
 
 activation_potential = 0
 class_count = 2
 
-x_train, y_train = pbi.synthetic_augmentation(x_train, y_train, class_count)
-x_test, y_test = pbi.synthetic_augmentation(x_test, y_test, class_count)
+x_train, y_train = pdi.auto_balancer(x_train, y_train, class_count)
+x_test, y_test = pdi.synthetic_augmentation(x_test, y_test, class_count)
 
-W = pbi.fit(x_train, y_train, activation_potential)
+W = pdi.fit(x_train, y_train)
 
 visualize = 'n'
 
-test_model = pbi.evaluate(x_test, y_test, activation_potential, visualize, W)
+test_model = pdi.evaluate(x_test, y_test, visualize, W)
 
-test_preds = test_model[pbi.get_preds()]
-test_acc = test_model[pbi.get_acc()]
+test_preds = test_model[pdi.get_preds()]
+test_acc = test_model[pdi.get_acc()]
 
 model_name = 'creditcard_fraud'
 model_type = 'PLAN'
@@ -49,7 +49,7 @@ weights_type = 'txt'
 weights_format = 'd'
 model_path = 'PlanModels/'
 
-pbi.save_model(model_name, model_type, class_count, activation_potential, test_acc, weights_type, weights_format, model_path, W)
+pdi.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, W)
 
 
 y_test = np.argmax(y_test, axis=1)
