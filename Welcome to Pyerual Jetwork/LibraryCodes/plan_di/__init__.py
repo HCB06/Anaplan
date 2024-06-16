708e0571dbc9314f7c51c4a368a1d4da7c2e0107
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-16 04:09:07 +0300

    Delete Welcome to Pyerual Jetwork/LibraryCodes directory

--- Welcome to Pyerual Jetwork/LibraryCodes/plan_di/__init__.py
+++
@@ -1,5 +0,0 @@
-# pyerualjetwork/PLAN/__init__.py
-
-# Bu dosya, plan modülünün ana giriş noktasıdır.
-
-from .plan_di import auto_balancer, normalization, Softmax, Sigmoid, Relu, synaptic_pruning, synaptic_dividing, weight_identification, fex, cat, fit, evaluate, save_model, load_model, predict_model_ssd, predict_model_ram, get_weights, get_df, get_preds, get_acc, synthetic_augmentation, standard_scaler, multiple_evaluate
