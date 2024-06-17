de6350eb2de66ac93548d201a3830c17a5ba49a8
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-17 17:18:41 +0300

    Add files via upload

---
+++ Welcome to Pyerual Jetwork/pyerualjetwork-2.2.5/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.2.5",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Code improvements (Documentation in desc. Examples in GİTHUB: https://github.com/HCB06/PyerualJetwork)",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks'],
+
+      
+      )
