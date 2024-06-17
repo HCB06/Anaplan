f0b42fdbcabca659c01c28aed6cc5c7ff02c5c97
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-17 11:53:18 +0300

    Update train.m

--- Welcome to PLAN/Codes/DirectInjectionPLAN/train.m
+++ Welcome to PLAN/Codes/DirectInjectionPLAN/train.m
@@ -23,62 +23,6 @@ weights1 = ones(9,784); % Matrix formed by ones
 weights2 = eye(9); % Diagonal matrix
 
 
-if class == 1 
-
-	weights1(2:9,:) = 0; % All except the first neuron are zero (synaptic pruning) 
-
-	elseif class == 2 
-
-		weights1(1,:) = 0;
-		weights1(3:9,:) = 0;
-
-		elseif class == 3
-
-			weights1(1:2,:) = 0;
-			weights1(4:9,:) = 0;
-
-			elseif class == 4
-
-				weights1(1:3,:) = 0;
-				weights1(5:9,:) = 0;
-				 
-				elseif class == 5 
-
-					weights1(1:4,:) = 0;
-					weights1(6:9,:) = 0;
-     
-					elseif class == 6 
-
-						weights1(1:5,:) = 0;
-						weights1(7:9,:) = 0;
-
-						elseif class == 7
-
-							weights1(1:6,:) = 0;
-							weights1(8:9,:) = 0;
-
-							elseif class == 8 
-
-								weights1(1:7,:) = 0;
-								weights1(9,:) = 0;
-
-								elseif class == 9 
-
-									weights1(1:8,:) = 0;
-									
-end
-
-%{
-or you can do this (its more simple):
-
-weights1 = zeros(9,784); % Matrix formed by zeros
-weights2 = eye(9); % Matrix formed by zeros
-
-weights1(class,:) = 1;
- 
-
-%}
-
 %% FEATURE EXTRACTION LAYER %%
   
   weights1(class,:) = inputLayer;
