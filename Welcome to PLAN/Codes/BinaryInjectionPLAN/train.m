40c556a20f3082645c28b3ca26a89cb2374d1edf
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-17 16:34:00 +0300

    Update train.m

--- Welcome to PLAN/Codes/BinaryInjectionPLAN/train.m
+++ Welcome to PLAN/Codes/BinaryInjectionPLAN/train.m
@@ -24,67 +24,13 @@ weights1 = ones(9,784); % Matrix formed by ones
 weights2 = eye(9); % Diagonal matrix
 
 
-if class == 1 
-
-	weights1(2:9,:) = 0; % All except the first neuron are zero (synaptic pruning) 
-
-	elseif class == 2 
-
-		weights1(1,:) = 0;
-		weights1(3:9,:) = 0;
-
-		elseif class == 3
-
-			weights1(1:2,:) = 0;
-			weights1(4:9,:) = 0;
-
-			elseif class == 4
-
-				weights1(1:3,:) = 0;
-				weights1(5:9,:) = 0;
-				 
-				elseif class == 5 
-
-					weights1(1:4,:) = 0;
-					weights1(6:9,:) = 0;
-     
-					elseif class == 6 
-
-						weights1(1:5,:) = 0;
-						weights1(7:9,:) = 0;
-
-						elseif class == 7
-
-							weights1(1:6,:) = 0;
-							weights1(8:9,:) = 0;
-
-							elseif class == 8 
-
-								weights1(1:7,:) = 0;
-								weights1(9,:) = 0;
-
-								elseif class == 9 
-
-									weights1(1:8,:) = 0;
-									
-end
-
-%{
-or you can do this (its more simple):
-
-weights1 = zeros(9,784); % Matrix formed by zeros
-weights2 = eye(9); % Matrix formed by zeros
-
-weights1(class,:) = 1;
- 
-
-%}
-
 inputLayer = normalization(inputLayer); % inputs in range 0 - 1
 
 %% FEATURE EXTRACTION LAYER %%
-  inputConnections = find(inputLayer < activationPotential);
-  weights1(:,inputConnections(:)) = 0;
+ inputLayer(inputLayer < activation_potential) = 0
+ inputLayer(inputLayer > activation_potential) = 1
+ weights1(class,:) = inputLayer;
+
 
 if class ~= 1
 	
