0de8771d38b51f56e4dbf588786dfbde57bf2136
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-17 12:24:11 +0300

    Add files via upload

---
+++ Welcome to Pyerual Jetwork/pyerualjetwork-2.2.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.2.2",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= " 8 new functions: multiple_evaluate , encode_one_hot, split, metrics, decode_one_hot, roc_curve, confusion_matrix, plot_evaluate And Code improvements (Documentation in desc. Examples in GİTHUB: https://github.com/HCB06/PyerualJetwork)",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks'],
+
+      
+      )
