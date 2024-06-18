53238e358c839e2e43851bf1dd0c779e05683522
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-18 23:29:22 +0300

    Update plan(cancer).py

--- Welcome to Pyerual Jetwork/ExampleCodes/plan(cancer).py
+++ Welcome to Pyerual Jetwork/ExampleCodes/plan(cancer).py
@@ -13,14 +13,13 @@ x_train, x_test, y_train, y_test = pdi.split(X, y, 0.4, 42)
 x_train = x_train.tolist()
 x_test = x_test.tolist()
 
-x_train, x_test = pdi.standard_scaler(x_train, x_test)
-scaler = True
+
+scaler_params, x_train, x_test = pdi.standard_scaler(x_train, x_test)
 
 y_train, y_test = pdi.encode_one_hot(y_train, y_test)
 
 show_metrics = True
 
-x_train, y_train = pdi.auto_balancer(x_train, y_train)
 x_test, y_test = pdi.auto_balancer(x_test, y_test)
 
 W = pdi.fit(x_train, y_train)
@@ -32,11 +31,13 @@ test_acc = test_model[pdi.get_acc()]
 model_name = 'breast_cancer'
 model_type = 'PLAN'
 weights_type = 'txt'
-weights_format = 'd'
+weights_format = 'raw'
 model_path = 'PlanModels/'
 class_count = 2
 
-pdi.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler, W)
+pdi.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler_params, W)
+
+
 
 precisison, recall, f1 = pdi.metrics(y_test, test_preds)
 print('Precision: ', precisison, '\n', 'Recall: ', recall, '\n', 'F1: ', f1)
@@ -45,7 +46,7 @@ y_test = pdi.decode_one_hot(y_test)
 
 
 for i in range(len(x_test)):
-    Predict = pdi.predict_model_ram(x_test[i], scaler, W)
+    Predict = pdi.predict_model_ram(x_test[i], scaler_params, W)
     time.sleep(0.6)
     if np.argmax(Predict) == y_test[i]:
         print(Fore.GREEN + 'Predicted Output(index):', np.argmax(Predict), 'Real Output(index):', y_test[i])
