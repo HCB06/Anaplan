01d3a94229f0a78cb046b21edb43e8015205f52f
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-19 16:57:56 +0300

    Add files via upload

---
+++ Welcome to Pyerual Jetwork/pyerualjetwork-2.4.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.4.6",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "show_training parameter added for fit function[True, 'final' or any]. Code improvements",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks'],
+
+      
+      )
