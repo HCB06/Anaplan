6b6026bd3845087092bdc4b6b1af9adb266c5910
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-20 17:50:06 +0300

    Update iris(plan).py

--- Welcome to Pyerual Jetwork/ExampleCodes/iris(plan).py
+++ Welcome to Pyerual Jetwork/ExampleCodes/iris(plan).py
@@ -1,6 +1,6 @@
 import time
 from colorama import Fore
-import plan_di as pdi
+import plan
 from sklearn.datasets import load_iris
 import numpy as np
 
@@ -8,26 +8,26 @@ data = load_iris()
 X = data.data
 y = data.target
 
-x_train, x_test, y_train, y_test = pdi.split(X, y, 0.1, 42)
+x_train, x_test, y_train, y_test = plan.split(X, y, 0.1, 42)
 
 x_train = x_train.tolist()
 x_test = x_test.tolist()
 
-scaler_params, x_train, x_test = pdi.standard_scaler(x_train, x_test)
+scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
 
-y_train, y_test = pdi.encode_one_hot(y_train, y_test)
+y_train, y_test = plan.encode_one_hot(y_train, y_test)
 
 show_metrics = True
 show_training = True # other values: 'final' or None(any)
 
-x_train, y_train = pdi.auto_balancer(x_train, y_train)
-x_test, y_test = pdi.auto_balancer(x_test, y_test)
+x_train, y_train = plan.auto_balancer(x_train, y_train)
+x_test, y_test = plan.auto_balancer(x_test, y_test)
 
-W = pdi.fit(x_train, y_train, show_training)
+W = plan.fit(x_train, y_train, show_training)
 
-test_model = pdi.evaluate(x_test, y_test, show_metrics, W)
-test_preds = test_model[pdi.get_preds()]
-test_acc = test_model[pdi.get_acc()]
+test_model = plan.evaluate(x_test, y_test, show_metrics, W)
+test_preds = test_model[plan.get_preds()]
+test_acc = test_model[plan.get_acc()]
 
 model_name = 'iris'
 model_type = 'PLAN'
@@ -36,16 +36,16 @@ weights_format = 'd'
 model_path = 'PlanModels/'
 class_count = 3
 
-pdi.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler_params, W)
+plan.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler_params, W)
 
-precisison, recall, f1 = pdi.metrics(y_test, test_preds)
+precisison, recall, f1 = plan.metrics(y_test, test_preds)
 print('Precision: ', precisison, '\n', 'Recall: ', recall, '\n', 'F1: ', f1)
 
-y_test = pdi.decode_one_hot(y_test)
+y_test = plan.decode_one_hot(y_test)
 
 """
 for i in range(len(x_test)):
-    Predict = pdi.predict_model_ram(x_test[i], scaler, W)
+    Predict = plan.predict_model_ram(x_test[i], scaler, W)
     time.sleep(0.6)
     if np.argmax(Predict) == y_test[i]:
         print(Fore.GREEN + 'Predicted Output(index):', np.argmax(Predict), 'Real Output(index):', y_test[i])
