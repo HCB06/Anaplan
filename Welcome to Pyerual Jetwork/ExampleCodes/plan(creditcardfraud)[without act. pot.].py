ecc5778012ba3388ef0c3fb46d592a1c25104477
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-20 18:38:14 +0300

    Update and rename plan_direct_injection(creditcardfraud).py to plan(creditcardfraud)[without act. pot.].py

--- Welcome to Pyerual Jetwork/ExampleCodes/plan(creditcardfraud)[without act. pot.].py
+++ Welcome to Pyerual Jetwork/ExampleCodes/plan(creditcardfraud)[without act. pot.].py
@@ -1,5 +1,5 @@
 import pandas as pd
-import plan_di as pdi
+import plan
 import numpy as np
 
 data = pd.read_csv('creditcard.csv')
@@ -8,9 +8,9 @@ x = data.drop('Class', axis=1).values
 y = data['Class'].values
 
 
-x_train, x_test, y_train, y_test = pdi.split(x, y, 0.4, 42)
+x_train, x_test, y_train, y_test = plan.split(x, y, 0.4, 42)
 
-y_train, y_test = pdi.encode_one_hot(y_train, y_test)
+y_train, y_test = plan.encode_one_hot(y_train, y_test)
 
 x_train = x_train.tolist()
 x_test = x_test.tolist()
@@ -19,19 +19,19 @@ class_count = 2
 show_metrics = True
 show_training = None # other values: 'final' or True
 
-scaler_params, x_train, x_test = pdi.standard_scaler(x_train, x_test)
+scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
 
-x_train, y_train = pdi.auto_balancer(x_train, y_train)
-x_test, y_test = pdi.synthetic_augmentation(x_test, y_test)
-
-
-W = pdi.fit(x_train, y_train, show_training)
+x_train, y_train = plan.auto_balancer(x_train, y_train)
+x_test, y_test = plan.synthetic_augmentation(x_test, y_test)
 
 
-test_model = pdi.evaluate(x_test, y_test, show_metrics, W)
+W = plan.fit(x_train, y_train, show_training)
 
-test_preds = test_model[pdi.get_preds()]
-test_acc = test_model[pdi.get_acc()]
+
+test_model = plan.evaluate(x_test, y_test, show_metrics, W)
+
+test_preds = test_model[plan.get_preds()]
+test_acc = test_model[plan.get_acc()]
 
 model_name = 'creditcard_fraud(di)'
 model_type = 'PLAN'
@@ -39,7 +39,7 @@ weights_type = 'txt'
 weights_format = 'd'
 model_path = 'PlanModels'
 
-pdi.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler_params, W)
+plan.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler_params, W)
 
 
 
@@ -57,6 +57,6 @@ for i in range(len(x_test)):
 
 """
 
-precisison, recall, f1 = pdi.metrics(y_test, test_preds)
+precisison, recall, f1 = plan.metrics(y_test, test_preds)
 
 print('Precision: ', precisison, '\n', 'Recall: ', recall, '\n', 'F1: ', f1)
