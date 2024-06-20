722c927dee7523aa32ce241359db740ffaf79902
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-20 18:33:39 +0300

    Update plan(wine).py

--- Welcome to Pyerual Jetwork/ExampleCodes/plan(wine).py
+++ Welcome to Pyerual Jetwork/ExampleCodes/plan(wine).py
@@ -7,7 +7,7 @@ Created on Tue Jun 18 23:32:16 2024
 
 import time
 from colorama import Fore
-import plan_di as pdi
+import plan
 from sklearn.datasets import load_wine
 import numpy as np
 
@@ -15,26 +15,26 @@ data = load_wine()
 X = data.data
 y = data.target
 
-x_train, x_test, y_train, y_test = pdi.split(X, y, 0.6, 42)
+x_train, x_test, y_train, y_test = plan.split(X, y, 0.6, 42)
 
 x_train = x_train.tolist()
 x_test = x_test.tolist()
 
-scaler_params, x_train, x_test = pdi.standard_scaler(x_train, x_test)
+scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
 
 
-y_train, y_test = pdi.encode_one_hot(y_train, y_test)
+y_train, y_test = plan.encode_one_hot(y_train, y_test)
 
 show_metrics = True
 show_training = None
 
-x_test, y_test = pdi.auto_balancer(x_test, y_test)
+x_test, y_test = plan.auto_balancer(x_test, y_test)
 
-W = pdi.fit(x_train, y_train, show_training)
+W = plan.fit(x_train, y_train, show_training)
 
-test_model = pdi.evaluate(x_test, y_test, show_metrics, W)
-test_preds = test_model[pdi.get_preds()]
-test_acc = test_model[pdi.get_acc()]
+test_model = plan.evaluate(x_test, y_test, show_metrics, W)
+test_preds = test_model[plan.get_preds()]
+test_acc = test_model[plan.get_acc()]
 
 model_name = 'wine'
 model_type = 'PLAN'
@@ -43,17 +43,17 @@ weights_format = 'd'
 model_path = 'PlanModels/'
 class_count = 3
 
-pdi.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler_params, W)
+plan.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler_params, W)
 
-precisison, recall, f1 = pdi.metrics(y_test, test_preds)
+precisison, recall, f1 = plan.metrics(y_test, test_preds)
 print('Precision: ', precisison, '\n', 'Recall: ', recall, '\n', 'F1: ', f1)
 
-y_test = pdi.decode_one_hot(y_test)
+y_test = plan.decode_one_hot(y_test)
 
 
 scaler_params = None # Because test data is already scaled. The real world input must be scale.
 for i in range(len(x_test)):
-    Predict = pdi.predict_model_ram(x_test[i], scaler_params, W)
+    Predict = plan.predict_model_ram(x_test[i], scaler_params, W)
     time.sleep(0.6)
     if np.argmax(Predict) == y_test[i]:
         print(Fore.GREEN + 'Predicted Output(index):', np.argmax(Predict), 'Real Output(index):', y_test[i])
