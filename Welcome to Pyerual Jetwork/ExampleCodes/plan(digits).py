f2d1766f472dd32befa71b4c65c9a051fae3b609
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-20 18:15:03 +0300

    Update plan(digits).py

--- Welcome to Pyerual Jetwork/ExampleCodes/plan(digits).py
+++ Welcome to Pyerual Jetwork/ExampleCodes/plan(digits).py
@@ -1,6 +1,6 @@
 import time
 from colorama import Fore, Style
-import plan_di as pdi
+import plan
 from sklearn.datasets import load_digits
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,28 +12,28 @@ X = data.data
 y = data.target
 
     
-x_train, x_test, y_train, y_test = pdi.split(X, y, 0.4, 42)
+x_train, x_test, y_train, y_test = plan.split(X, y, 0.4, 42)
     
     
 x_train = x_train.tolist()
 x_test = x_test.tolist()
 
 
-y_train, y_test = pdi.encode_one_hot(y_train, y_test)
+y_train, y_test = plan.encode_one_hot(y_train, y_test)
 
 show_metrics = True
 scaler_params = None
 class_count = 10
 show_training = True
 
-x_test, y_test = pdi.auto_balancer(x_test, y_test)
+x_test, y_test = plan.auto_balancer(x_test, y_test)
 
 
-W = pdi.fit(x_train, y_train, show_training)
+W = plan.fit(x_train, y_train, show_training)
 
-test_model = pdi.evaluate(x_test, y_test, show_metrics, W)
-test_preds = test_model[pdi.get_preds()]
-test_acc = test_model[pdi.get_acc()]
+test_model = plan.evaluate(x_test, y_test, show_metrics, W)
+test_preds = test_model[plan.get_preds()]
+test_acc = test_model[plan.get_acc()]
 
 model_name = 'digits'
 model_type = 'PLAN'
@@ -41,16 +41,16 @@ weights_type = 'txt'
 weights_format = 'd'
 model_path = 'PlanModels'
 
-pdi.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler_params, W)
+plan.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler_params, W)
 
-precisison, recall, f1 = pdi.metrics(y_test, test_preds)
+precisison, recall, f1 = plan.metrics(y_test, test_preds)
 print('Precision: ', precisison, '\n', 'Recall: ', recall, '\n', 'F1: ', f1)
 
-y_test = pdi.decode_one_hot(y_test)
+y_test = plan.decode_one_hot(y_test)
 
 """
 for i in range(len(x_test)):
-    Predict = pdi.predict_model_ram(x_test[i], scaler, W)
+    Predict = plan.predict_model_ram(x_test[i], scaler, W)
     time.sleep(0.6)
     if np.argmax(Predict) == y_test[i]:
         print(Fore.GREEN + 'Predicted Output(index):', np.argmax(Predict), 'Real Output(index):', y_test[i])
