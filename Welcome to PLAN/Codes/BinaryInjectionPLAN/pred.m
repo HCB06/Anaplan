f441cfeb9b2cd6d4df2dc32d140af532d0befb11
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-22 22:42:15 +0300

    Update pred.m

--- Welcome to PLAN/Codes/BinaryInjectionPLAN/pred.m
+++ Welcome to PLAN/Codes/BinaryInjectionPLAN/pred.m
@@ -1,8 +1,8 @@
-function [output_layer] = pred(inputLayer, activationPotential, weights1, weights2)
+function [output_layer] = pred(inputLayer, activationPotentiation, weights1, weights2)
 
 inputLayer = normalization(inputLayer); % inputs in range 0 - 1
-inputLayer(inputLayer < activationPotential) = 0;
-inputLayer(inputLayer > activationPotential) = 1;
+inputLayer(inputLayer < activationPotentiation) = 0;
+inputLayer(inputLayer > activationPotentiation) = 1;
 featureLayer = weights1 * inputLayer;
 
 catalystLayer = weights2 * featureLayer;
