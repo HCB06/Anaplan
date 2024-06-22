c69500635bbe70410316a2e1fde480c094abae51
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-22 23:33:39 +0300

    Add files via upload

---
+++ Welcome to Pyerual Jetwork/pyerualjetwork-2.6.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.6.2",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "New optional parameters added for fit function, x_val, y_val show_count, val_count, val. For more information please read user document",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'pruning learning artficial neural networks'],
+
+      
+      )
