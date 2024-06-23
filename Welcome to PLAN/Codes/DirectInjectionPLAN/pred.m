196fd0bf21bc1233a325c07e1bfabfeffb0a274d
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-24 00:11:18 +0300

    Update pred.m

--- Welcome to PLAN/Codes/DirectInjectionPLAN/pred.m
+++ Welcome to PLAN/Codes/DirectInjectionPLAN/pred.m
@@ -1,6 +1,5 @@
-function [output_layer] = pred(inputLayer, weights1, weights2)
+function [output_layer] = pred(inputLayer, weights1)
 
 featureLayer = weights1 * inputLayer;
-catalystLayer = weights2 * featureLayer;
 
-output_layer = catalystLayer;
+output_layer = featureLayer;
