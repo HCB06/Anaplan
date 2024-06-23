13c649624781c6dc1d07a6d8136dfedfa4973732
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-06-24 00:15:09 +0300

    Update delete_model.m

--- Welcome to PLAN/Codes/BinaryInjectionPLAN/delete_model.m
+++ Welcome to PLAN/Codes/BinaryInjectionPLAN/delete_model.m
@@ -3,25 +3,13 @@ function delete_model ()
 		fileName1 = sprintf('weights/weights1.mat');
     load(fileName1);
     
-		fileName2 = sprintf('weights/weights2.mat');
-    load(fileName2);
-	
 	
 	
 	weights1(:,:) = 0;
-	
-	weights2(:,:) = 0;
-	
 	
 	
 		fileName1 = sprintf('weights/weights1.mat');
 	save(fileName1, 'weights1');
 
-		fileName2 = sprintf('weights/weights2.mat');
-	save(fileName2, 'weights2');  
-	
+
 	disp('Model deleted. starting training..');
-	
-	pause(1);
-	
-	train()
