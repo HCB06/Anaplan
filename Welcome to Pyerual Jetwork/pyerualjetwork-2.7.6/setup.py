93d1e60ef050858765ac46573041dffd35ba7907
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-07-02 21:45:04 +0300

    Add files via upload

---
+++ Welcome to Pyerual Jetwork/pyerualjetwork-2.7.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "2.7.6",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Changes in standard_scaler function paramter: x_test default value=None. Now x_test parameter is optional",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'potentiation learning artficial neural networks'],
+
+      
+      )
