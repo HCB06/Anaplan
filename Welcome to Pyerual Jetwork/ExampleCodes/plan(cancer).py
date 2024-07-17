5e1df29da84d191848d55f75484ae31e0e77ccef
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-07-17 16:29:40 +0300

    Update plan(cancer).py

--- Welcome to Pyerual Jetwork/ExampleCodes/plan(cancer).py
+++ Welcome to Pyerual Jetwork/ExampleCodes/plan(cancer).py
@@ -1,8 +1,5 @@
-import time
-from colorama import Fore
 import plan
 from sklearn.datasets import load_breast_cancer
-import numpy as np
 
 data = load_breast_cancer()
 X = data.data
@@ -10,22 +7,18 @@ y = data.target
 
 x_train, x_test, y_train, y_test = plan.split(X, y, 0.4, 42)
 
-x_train = x_train.tolist()
-x_test = x_test.tolist()
-
-
 scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
 
 y_train, y_test = plan.encode_one_hot(y_train, y_test)
 
-show_metrics = True
-show_training = None
-class_count = 2
 x_test, y_test = plan.auto_balancer(x_test, y_test)
 
-W = plan.fit(x_train, y_train, show_training)
-W = plan.weight_normalization(W, class_count)
-test_model = plan.evaluate(x_test, y_test, show_metrics, W)
+W = plan.fit(x_train, y_train)
+
+W = plan.normalization(W)
+
+test_model = plan.evaluate(x_test, y_test, show_metrices=True, W=W)
+
 test_preds = test_model[plan.get_preds()]
 test_acc = test_model[plan.get_acc()]
 
@@ -33,7 +26,7 @@ model_name = 'breast_cancer'
 model_type = 'PLAN'
 weights_type = 'txt'
 weights_format = 'f'
-model_path = 'PlanModels/'
+model_path = ''
 class_count = 2
 
 
@@ -44,16 +37,3 @@ plan.save_model(model_name, model_type, class_count, test_acc, weights_type, wei
 
 precisison, recall, f1 = plan.metrics(y_test, test_preds)
 print('Precision: ', precisison, '\n', 'Recall: ', recall, '\n', 'F1: ', f1)
-
-y_test = plan.decode_one_hot(y_test)
-
-
-for i in range(len(x_test)):
-    Predict = plan.predict_model_ssd(x_test[i], model_name, model_path)
-    time.sleep(0.6)
-    if np.argmax(Predict) == y_test[i]:
-        print(Fore.GREEN + 'Predicted Output(index):', np.argmax(Predict), 'Real Output(index):', y_test[i])
-    else:
-        print(Fore.RED + 'Predicted Output(index):', np.argmax(Predict), 'Real Output(index):', y_test[i])
-        
-        
