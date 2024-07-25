306b6fcf59f14e9469ec1ba48b87d866f1a2fa8c
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-07-25 21:57:54 +0300

    Update plan(wine).py

--- Welcome to Pyerual Jetwork/ExampleCodes/plan(wine).py
+++ Welcome to Pyerual Jetwork/ExampleCodes/plan(wine).py
@@ -17,22 +17,15 @@ y = data.target
 
 x_train, x_test, y_train, y_test = plan.split(X, y, 0.6, 42)
 
-x_train = x_train.tolist()
-x_test = x_test.tolist()
-
 scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
-
 
 y_train, y_test = plan.encode_one_hot(y_train, y_test)
 
-show_metrics = True
-show_training = None
-
 x_test, y_test = plan.auto_balancer(x_test, y_test)
 
-W = plan.fit(x_train, y_train, show_training)
+W = plan.fit(x_train, y_train)
 
-test_model = plan.evaluate(x_test, y_test, show_metrics, W)
+test_model = plan.evaluate(x_test, y_test, show_metrices=True, W=W)
 test_preds = test_model[plan.get_preds()]
 test_acc = test_model[plan.get_acc()]
 
@@ -40,24 +33,10 @@ model_name = 'wine'
 model_type = 'PLAN'
 weights_type = 'txt'
 weights_format = 'd'
-model_path = 'PlanModels/'
+model_path = ''
 class_count = 3
 
 plan.save_model(model_name, model_type, class_count, test_acc, weights_type, weights_format, model_path, scaler_params, W)
 
 precisison, recall, f1 = plan.metrics(y_test, test_preds)
 print('Precision: ', precisison, '\n', 'Recall: ', recall, '\n', 'F1: ', f1)
-
-y_test = plan.decode_one_hot(y_test)
-
-
-scaler_params = None # Because test data is already scaled. The real world input must be scale.
-for i in range(len(x_test)):
-    Predict = plan.predict_model_ram(x_test[i], scaler_params, W)
-    time.sleep(0.6)
-    if np.argmax(Predict) == y_test[i]:
-        print(Fore.GREEN + 'Predicted Output(index):', np.argmax(Predict), 'Real Output(index):', y_test[i])
-    else:
-        print(Fore.RED + 'Predicted Output(index):', np.argmax(Predict), 'Real Output(index):', y_test[i])
-        
-        
