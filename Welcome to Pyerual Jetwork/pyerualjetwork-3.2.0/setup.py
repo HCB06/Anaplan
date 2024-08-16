4287838d185574e1ca62fb044e01ea125b44ebce
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-08-16 05:03:26 +0300

    Add files via upload

---
+++ Welcome to Pyerual Jetwork/pyerualjetwork-3.2.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "3.2.0",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Deep PLAN Integreted. Version 3 document coming",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'potentiation learning artficial neural networks'],
+
+      
+      )
