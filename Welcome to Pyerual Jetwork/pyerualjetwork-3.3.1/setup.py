713efa8812c55a4d259271009956e17f97e74165
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-08-19 01:40:42 +0300

    Add files via upload

---
+++ Welcome to Pyerual Jetwork/pyerualjetwork-3.3.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "pyerualjetwork",
+      version = "3.3.1",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "fit function changes: LTD parameter included for Deep PLAN and professional visualizing for training. in the fit funciton, show_training=True",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'potentiation learning artficial neural networks'],
+
+      
+      )
