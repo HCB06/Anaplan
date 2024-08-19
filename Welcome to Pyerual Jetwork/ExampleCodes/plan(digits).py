853aeaf52c74cc19d13176c156fd04156f06dea6
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-08-20 02:17:10 +0300

    Update plan(digits).py

--- Welcome to Pyerual Jetwork/ExampleCodes/plan(digits).py
+++ Welcome to Pyerual Jetwork/ExampleCodes/plan(digits).py
@@ -13,23 +13,21 @@ X = data.data
 y = data.target
 
 X = plan.normalization(X)
-    
-x_train, x_test, y_train, y_test = plan.split(X, y, 0.4, 42)
 
+x_train, x_test, y_train, y_test = plan.split(X, y, 0.4, 42)
 
 y_train, y_test = plan.encode_one_hot(y_train, y_test)
 
 
 x_test, y_test = plan.auto_balancer(x_test, y_test)
 
-plan.plot_decision_space(x_train, plan.decode_one_hot(y_train))
-plt.show()
+activation_porentiation=['bent_identity']
 
-W = plan.fit(x_train, y_train)
+W = plan.fit(x_train, y_train, activation_potentiation=activation_porentiation)
 
 # TEST
 
-test_model = plan.evaluate(x_train, y_train, show_metrices=True, W=W)
+test_model = plan.evaluate(x_test, y_test, show_metrices=True, W=W, activation_potentiation=activation_porentiation)
 test_preds = test_model[plan.get_preds()]
 test_acc = test_model[plan.get_acc()]
 
