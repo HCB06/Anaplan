d136c86c1a9443bffd134d8a3fc2a816f632c661
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-08-20 02:37:49 +0300

    Update plan(cancer).py

--- Welcome to Pyerual Jetwork/ExampleCodes/plan(cancer).py
+++ Welcome to Pyerual Jetwork/ExampleCodes/plan(cancer).py
@@ -17,23 +17,23 @@ y = data.target
 
 # Eğitim, test ve doğrulama verilerini ayırma
 x_train, x_test, y_train, y_test = plan.split(X, y, 0.4, 42)
+
 x_train, x_val, y_train, y_val = plan.split(x_train, y_train, 0.2, 42)
 
 # One-hot encoding işlemi
 y_train, y_test = plan.encode_one_hot(y_train, y_test)
 y_val = plan.encode_one_hot(y_val, y)[0]
 
-# Veri dengesizliği durumunda otomatik dengeleme
 x_train, y_train = plan.auto_balancer(x_train, y_train)
+x_test, y_test = plan.auto_balancer(x_test, y_test)
 
-# Verilerin standardize edilmesi
 scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
 
-# Aktivasyon fonksiyonları
-activation_potentiation = ['waveakt', None, None]
+activation_potentiation = ['tanh']
 
 # Modeli eğitme
-W = plan.fit(x_train, y_train, activation_potentiation=activation_potentiation)
+W = plan.fit(x_train, y_train, activation_potentiation=activation_potentiation, LTD=2)
+
 
 # Modeli test etme
 test_model = plan.evaluate(x_test, y_test, show_metrices=True, W=W, activation_potentiation=activation_potentiation)
