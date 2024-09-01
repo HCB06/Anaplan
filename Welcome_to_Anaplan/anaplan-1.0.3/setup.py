91a3f31e361eb6e68850abd927f6fd606980e1b2
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-09-01 06:04:35 +0300

    Add files via upload

---
+++ Welcome_to_Anaplan/anaplan-1.0.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "anaplan",
+      version = "1.0.3",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "Bug Fix",
+      packages = find_packages(),
+      keywords = ["model evaluation", "classifcation", 'potentiation learning artficial neural networks'],
+
+      
+      )
