012408c7040719b02d94f97a5e504a5c63cfa140
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-09-15 20:14:10 +0300

    Update plan(wine).py

--- Welcome_to_Anaplan/ExampleCodes/plan(wine).py
+++ Welcome_to_Anaplan/ExampleCodes/plan(wine).py
@@ -15,17 +15,19 @@ data = load_wine()
 X = data.data
 y = data.target
 
-x_train, x_test, y_train, y_test = plan.split(X, y, 0.6, 42)
-
-scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
+x_train, x_test, y_train, y_test = plan.split(X, y, 0.4, 42)
 
 y_train, y_test = plan.encode_one_hot(y_train, y_test)
 
 x_test, y_test = plan.auto_balancer(x_test, y_test)
 
-W = plan.fit(x_train, y_train)
+scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
 
-test_model = plan.evaluate(x_test, y_test, show_metrices=True, W=W)
+act_pot = plan.activation_optimizer(x_train, y_train, x_test, y_test, early_stop=True)
+
+W = plan.fit(x_train, y_train, activation_potentiation=act_pot)
+
+test_model = plan.evaluate(x_test, y_test, show_metrics=True, W=W, activation_potentiation=act_pot)
 test_preds = test_model[plan.get_preds()]
 test_acc = test_model[plan.get_acc()]
 
@@ -35,7 +37,7 @@ weights_type = 'txt'
 weights_format = 'raw'
 model_path = ''
 
-plan.save_model(model_name, model_type, test_acc, weights_type, weights_format, model_path, scaler_params, W)
+plan.save_model(model_name=model_name, model_type=model_type, test_acc=test_acc, weights_type=weights_type, weights_format=weights_format, model_path=model_path, scaler_params=scaler_params, W=W)
 
 precisison, recall, f1 = plan.metrics(y_test, test_preds)
 print('Precision: ', precisison, '\n', 'Recall: ', recall, '\n', 'F1: ', f1)
