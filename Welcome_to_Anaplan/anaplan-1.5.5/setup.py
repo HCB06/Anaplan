3b84c1a09151951d6c92600638c29ef9b7dfbf34
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-10-27 17:04:48 +0300

    Add files via upload

---
+++ Welcome_to_Anaplan/anaplan-1.5.5/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "anaplan",
+      version = "1.5.5",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "*'big_data_mode' parameter of learner function changed to 'auto_normalization'. Default=True.\n*'batch_size' parameter added for learner function.\nFor more details read learner functions doc string.: https://github.com/HCB06/Anaplan/blob/main/Welcome_to_Anaplan/ANAPLAN_USER_MANUEL_AND_LEGAL_INFORMATION(EN).pdf.",
+      packages=find_packages(),
+      package_data={
+        'anaplan': ['*.mp4']
+      },
+      include_package_data=True,
+      keywords = ["model evaluation", "classifcation", 'potentiation learning artficial neural networks'],
+
+      
+      )
