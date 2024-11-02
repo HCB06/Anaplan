8432763a7119ef6e131a14613e3161d74f0317f0
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-11-02 20:55:18 +0300

    Update creditcard_fraud(plan).py

--- Welcome_to_Anaplan/ExampleCodes/creditcard_fraud(plan).py
+++ Welcome_to_Anaplan/ExampleCodes/creditcard_fraud(plan).py
@@ -16,7 +16,7 @@ scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
 
 x_train, y_train = plan.synthetic_augmentation(x_train, y_train)
 
-model = plan.learner(x_train, y_train, x_test, y_test, target_acc=0.99) # learner function = TFL(Test Feedback Learning). If test parameters not given then uses Train Feedback. More information: https://github.com/HCB06/Anaplan/blob/main/Welcome_to_PLAN/PLAN.pdf
+model = plan.learner(x_train, y_train, x_test, y_test, batch_size=0.1, auto_normalization=False, target_acc=0.99) # learner function = TFL(Test Feedback Learning). If test parameters not given then uses Train Feedback. More information: https://github.com/HCB06/Anaplan/blob/main/Welcome_to_PLAN/PLAN.pdf
 
 activation_potentiation = model[plan.get_act_pot()]
 W = model[plan.get_weights()]
