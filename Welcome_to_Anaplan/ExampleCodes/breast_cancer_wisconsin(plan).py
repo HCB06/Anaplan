76f0104d5767a835444d11ee10413ab5b1505c52
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-12-21 09:03:27 +0300

    Update breast_cancer_wisconsin(plan).py

--- Welcome_to_Anaplan/ExampleCodes/breast_cancer_wisconsin(plan).py
+++ Welcome_to_Anaplan/ExampleCodes/breast_cancer_wisconsin(plan).py
@@ -6,7 +6,7 @@ Created on Thu Jun 20 02:36:20 2024
 """
 from colorama import Fore
 from sklearn.datasets import load_breast_cancer
-from anaplan import plan
+from anaplan import plan, data_manipulations, model_operations
 import numpy as np
 import time
 
@@ -15,20 +15,19 @@ data = load_breast_cancer()
 X = data.data
 y = data.target
 
-
 # Eğitim, test ve doğrulama verilerini ayırma
-x_train, x_test, y_train, y_test = plan.split(X, y, 0.4, 42)
+x_train, x_test, y_train, y_test = data_manipulations.split(X, y, 0.4, 42)
 
-x_train, x_val, y_train, y_val = plan.split(x_train, y_train, 0.2, 42)
+x_train, x_val, y_train, y_val = data_manipulations.split(x_train, y_train, 0.2, 42)
 
 # One-hot encoding işlemi
-y_train, y_test = plan.encode_one_hot(y_train, y_test)
-y_val = plan.encode_one_hot(y_val, y)[0]
+y_train, y_test = data_manipulations.encode_one_hot(y_train, y_test)
+y_val = data_manipulations.encode_one_hot(y_val, y)[0]
 
-x_train, y_train = plan.auto_balancer(x_train, y_train)
-x_test, y_test = plan.auto_balancer(x_test, y_test)
+x_train, y_train = data_manipulations.auto_balancer(x_train, y_train)
+x_test, y_test = data_manipulations.auto_balancer(x_test, y_test)
 
-scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
+scaler_params, x_train, x_test = data_manipulations.standard_scaler(x_train, x_test)
 
 #activation_potentiation = plan.activation_optimizer(x_train, y_train, x_test, y_test, early_stop=True)
 activation_potentiation = ['tanh']
@@ -41,7 +40,7 @@ W = plan.fit(x_train, y_train, activation_potentiation=activation_potentiation, 
 test_model = plan.evaluate(x_test, y_test, show_metrics=True, W=W, activation_potentiation=activation_potentiation)
 
 # Modeli kaydetme
-plan.save_model(model_name='breast_cancer',
+model_operations.save_model(model_name='breast_cancer',
                 test_acc=test_model[plan.get_acc()],
                 scaler_params=scaler_params,
                 activation_potentiation=activation_potentiation,
@@ -49,7 +48,7 @@ plan.save_model(model_name='breast_cancer',
 
 # Model tahminlerini değerlendirme
 for i in range(len(x_val)):
-    Predict = plan.predict_model_ssd(model_name='breast_cancer', model_path='', Input=x_val[i])
+    Predict = model_operations.predict_model_ssd(model_name='breast_cancer', model_path='', Input=x_val[i])
 
     time.sleep(0.5)
     if np.argmax(Predict) == np.argmax(y_val[i]):
