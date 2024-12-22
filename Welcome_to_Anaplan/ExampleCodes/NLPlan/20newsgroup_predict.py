1b9ef6d0c7c186d78fc038ff9fe95ffba932868e
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-12-22 21:27:19 +0300

    Update 20newsgroup_predict.py

--- Welcome_to_Anaplan/ExampleCodes/NLPlan/20newsgroup_predict.py
+++ Welcome_to_Anaplan/ExampleCodes/NLPlan/20newsgroup_predict.py
@@ -1,11 +1,11 @@
 import numpy as np
-from anaplan import plan
+from anaplan import model_operations, activation_functions
 import pickle
 
 with open('tfidf_20news.pkl', 'rb') as f:
     vectorizer = pickle.load(f)
 
-model = plan.load_model(model_name='20newsgroup', model_path='')
+model = model_operations.load_model(model_name='20newsgroup', model_path='')
 
 
 while True:
@@ -25,7 +25,7 @@ while True:
         inp = inp_vectorized.toarray()
 
         # Model ile tahmin yapma
-        predict = plan.predict_model_ram(Input=inp, W=model[plan.get_weights()], activation_potentiation=model[plan.get_act_pot()], scaler_params=model[plan.get_scaler()])
+        predict = model_operations.predict_model_ram(Input=inp, W=model[plan.get_weights()], activation_potentiation=model[plan.get_act_pot()], scaler_params=model[plan.get_scaler()])
 
 
         # Tahmini yorumlama
@@ -71,7 +71,7 @@ while True:
             predict_label = 'Din ve Diğer Tartışmalar'
             
 
-        print('%' + str(int(max(plan.Softmax(predict) * 100))) + ' ' + predict_label + '\n')
+        print('%' + str(int(max(activation_functions.Softmax(predict) * 100))) + ' ' + predict_label + '\n')
 
     except:
 
