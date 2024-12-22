74c735a3f26bf0187a1ddd7118b8b1352c9bc9c3
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-12-22 21:18:55 +0300

    Update wine(plan).py

--- Welcome_to_Anaplan/ExampleCodes/wine(plan).py
+++ Welcome_to_Anaplan/ExampleCodes/wine(plan).py
@@ -7,7 +7,7 @@ Created on Tue Jun 18 23:32:16 2024
 
 import time
 from colorama import Fore
-from anaplan import plan
+from anaplan import plan, data_operations, model_operations, metrics
 from sklearn.datasets import load_wine
 import numpy as np
 
@@ -15,13 +15,13 @@ data = load_wine()
 X = data.data
 y = data.target
 
-x_train, x_test, y_train, y_test = plan.split(X, y, 0.4, 42)
+x_train, x_test, y_train, y_test = data_operations.split(X, y, 0.4, 42)
 
-y_train, y_test = plan.encode_one_hot(y_train, y_test)
+y_train, y_test = data_operations.encode_one_hot(y_train, y_test)
 
-x_test, y_test = plan.auto_balancer(x_test, y_test)
+x_test, y_test = data_operations.auto_balancer(x_test, y_test)
 
-scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
+scaler_params, x_train, x_test = data_operations.standard_scaler(x_train, x_test)
 
 model = plan.learner(x_train, y_train, x_test, y_test, show_history=True, depth=2) # learner function = TFL(Test Feedback Learning). If test parameters not given then uses Train Feedback. More information: https://github.com/HCB06/Anaplan/blob/main/Welcome_to_PLAN/PLAN.pdf
 
@@ -36,7 +36,7 @@ test_acc = test_model[plan.get_acc()]
 model_name = 'wine'
 model_path = ''
 
-plan.save_model(model_name=model_name, activation_potentiation=model[plan.get_act_pot()], model_path=model_path, scaler_params=scaler_params, W=model[plan.get_weights()])
+model_operations.save_model(model_name=model_name, activation_potentiation=model[plan.get_act_pot()], model_path=model_path, scaler_params=scaler_params, W=model[plan.get_weights()])
 
-precisison, recall, f1 = plan.metrics(y_test, test_preds)
+precisison, recall, f1 = metrics(y_test, test_preds)
 print('Precision: ', precisison, '\n', 'Recall: ', recall, '\n', 'F1: ', f1)
