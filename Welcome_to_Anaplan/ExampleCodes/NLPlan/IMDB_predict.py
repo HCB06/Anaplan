d1c98d136e20c3b51e9c435cbf46ebf10bc08c58
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-12-22 21:28:10 +0300

    Update IMDB_predict.py

--- Welcome_to_Anaplan/ExampleCodes/NLPlan/IMDB_predict.py
+++ Welcome_to_Anaplan/ExampleCodes/NLPlan/IMDB_predict.py
@@ -1,11 +1,11 @@
 import numpy as np
-from anaplan import plan
+from anaplan import model_operations, activation_functions
 import pickle
 
 with open('tfidf_imdb.pkl', 'rb') as f:
     vectorizer = pickle.load(f)
 
-model = plan.load_model(model_name='IMDB', model_path='')
+model = model_operations.load_model(model_name='IMDB', model_path='')
 
 while True:
 
@@ -24,7 +24,7 @@ while True:
         inp = inp_vectorized.toarray()
 
         # Model ile tahmin yapma
-        predict = plan.predict_model_ram(Input=inp, W=model[plan.get_weights()], activation_potentiation=model[plan.get_act_pot()], scaler_params=model[plan.get_scaler()])
+        predict = model_operations.predict_model_ram(Input=inp, W=model[plan.get_weights()], activation_potentiation=model[plan.get_act_pot()], scaler_params=model[plan.get_scaler()])
 
 
         # Tahmini yorumlama
@@ -33,7 +33,7 @@ while True:
         elif np.argmax(predict) == 0:
             predict_label = 'negatif'
 
-        print('%' + str(int(max(plan.Softmax(predict) * 100))) + ' ' + predict_label + '\n')
+        print('%' + str(int(max(activation_functions.Softmax(predict) * 100))) + ' ' + predict_label + '\n')
 
     except:
 
