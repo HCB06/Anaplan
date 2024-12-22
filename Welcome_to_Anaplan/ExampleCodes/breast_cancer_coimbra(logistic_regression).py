a6c6235fcc60ed7ccadcb69e0969afdd2009ca0c
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2024-12-22 21:23:21 +0300

    Update breast_cancer_coimbra(logistic_regression).py

--- Welcome_to_Anaplan/ExampleCodes/breast_cancer_coimbra(logistic_regression).py
+++ Welcome_to_Anaplan/ExampleCodes/breast_cancer_coimbra(logistic_regression).py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report, accuracy_score
-import plan
+from anaplan import data_operations
 import numpy as np
 from matplotlib import pyplot as plt
 
@@ -14,20 +14,20 @@ X = data.drop('Classification', axis=1).values
 y = data['Classification'].values
 
 # Eğitim, test ve doğrulama verilerini ayırma
-x_train, x_test, y_train, y_test = plan.split(X, y, 0.4, 42)
-x_train, x_val, y_train, y_val = plan.split(x_train, y_train, 0.2, 42)
+x_train, x_test, y_train, y_test = data_operations.split(X, y, 0.4, 42)
+x_train, x_val, y_train, y_val = data_operations.split(x_train, y_train, 0.2, 42)
 
-y_val = plan.encode_one_hot(y_val, y_test)[0]
-y_train, y_test = plan.encode_one_hot(y_train, y_test)
+y_val = data_operations.encode_one_hot(y_val, y_test)[0]
+y_train, y_test = data_operations.encode_one_hot(y_train, y_test)
 
 # Veri dengesizliği durumunda otomatik dengeleme
-x_train, y_train = plan.auto_balancer(x_train, y_train)
+x_train, y_train = data_operations.auto_balancer(x_train, y_train)
 
-y_train = plan.decode_one_hot(y_train)
-y_test = plan.decode_one_hot(y_test)
+y_train = data_operations.decode_one_hot(y_train)
+y_test = data_operations.decode_one_hot(y_test)
 
 # Verilerin standardize edilmesi
-scaler_params, x_train, x_test = plan.standard_scaler(x_train, x_test)
+scaler_params, x_train, x_test = data_operations.standard_scaler(x_train, x_test)
 
 # Lojistik Regresyon modelini tanımlama ve eğitme
 model = LogisticRegression(random_state=42)
@@ -39,7 +39,7 @@ print(classification_report(y_test, y_pred))
 test_acc = accuracy_score(y_test, y_pred)
 print(f"Test Accuracy: {test_acc:.4f}")
 
-x_val = plan.standard_scaler(x_test=x_val, scaler_params=scaler_params)
+x_val = data_operations.standard_scaler(x_test=x_val, scaler_params=scaler_params)
 
 # Model tahminlerini değerlendirme
 y_val_pred = model.predict(x_val)
