2e3d6ea944b72a65b295b34b3f833415e7ab70d9
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2025-01-01 06:15:56 +0300

    Add files via upload

---
+++ Welcome_to_Anaplan/anaplan-2.4.9/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+# Setting Up
+
+setup(
+      
+      name = "anaplan",
+      version = "2.4.9",
+      author = "Hasan Can Beydili",
+      author_email = "tchasancan@gmail.com",
+      description= "* Changes: https://github.com/HCB06/Anaplan/blob/main/CHANGES\n* ANAPLAN document: https://github.com/HCB06/Anaplan/blob/main/Welcome_to_Anaplan/ANAPLAN_USER_MANUEL_AND_LEGAL_INFORMATION(EN).pdf.",
+      packages=find_packages(),
+      keywords = ["model evaluation", "classifcation", 'potentiation learning artficial neural networks, NEAT, genetic algorithms, reinforcement_learning'],
+
+      
+      )
