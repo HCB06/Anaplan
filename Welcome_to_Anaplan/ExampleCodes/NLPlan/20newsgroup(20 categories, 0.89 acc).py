6a539b603444ad341d100fcc669dff5e3b4eb5a3
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2025-01-03 00:57:08 +0300

    Update 20newsgroup(20 categories, 0.89 acc).py

--- Welcome_to_Anaplan/ExampleCodes/NLPlan/20newsgroup(20 categories, 0.89 acc).py
+++ Welcome_to_Anaplan/ExampleCodes/NLPlan/20newsgroup(20 categories, 0.89 acc).py
@@ -46,14 +46,14 @@ print('classes: %s' % (newsgroups.target_names))
 # PLAN Modeli
 model = plan.learner(x_train, y_train, x_test, y_test, depth=10, target_acc=0.89, big_data_mode=True) # learner function = TFL(Test Feedback Learning). If test parameters not given then uses Train Feedback. More information: https://github.com/HCB06/Anaplan/blob/main/Welcome_to_PLAN/PLAN.pdf
 
-test_acc = model[plan.get_acc()]
-test_preds = model[plan.get_preds()]
-W = model[plan.get_weights()]
-activation_potentiation = model[plan.get_act_pot()]
+test_acc = model[model_operations.get_acc()]
+test_preds = model[model_operations.get_preds()]
+W = model[model_operations.get_weights()]
+activation_potentiation = model[model_operations.get_act_pot()]
 
 # Modeli kaydetme
 model_operations.save_model(model_name='20newsgroup', test_acc=test_acc, scaler_params=scaler_params, W=W, activation_potentiation=activation_potentiation)
 
 print(Fore.GREEN + "\n------PLAN Modeli Sonuçları------" + Fore.RESET)
 print(f"PLAN Test Accuracy: {test_acc:.4f}")
-print(classification_report(data_operations.decode_one_hot(y_test), model[plan.get_preds()], target_names=newsgroups.target_names))
+print(classification_report(data_operations.decode_one_hot(y_test), model[model_operations.get_preds()], target_names=newsgroups.target_names))
