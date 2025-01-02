977cc870e6962b9461ac09d4ff82801038db2e2c
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2025-01-03 00:59:38 +0300

    Update imdb.py

--- Welcome_to_Anaplan/ExampleCodes/NLPlan/imdb.py
+++ Welcome_to_Anaplan/ExampleCodes/NLPlan/imdb.py
@@ -43,14 +43,14 @@ scaler_params, x_train, x_test = data_operations.standard_scaler(x_train, x_test
 
 model = plan.learner(x_train, y_train, x_test, y_test, target_acc=0.85, auto_normalization=False, except_this=['circular']) # learner function = TFL(Test Feedback Learning). If test parameters not given then uses Train Feedback. More information: https://github.com/HCB06/Anaplan/blob/main/Welcome_to_PLAN/PLAN.pdf
 
-W = model[plan.get_weights()]
-activation_potentiation = model[plan.get_act_pot()]
+W = model[model_operations.get_weights()]
+activation_potentiation = model[model_operations.get_act_pot()]
 
 test_model = plan.evaluate(x_test, y_test, W=W, activation_potentiation=activation_potentiation)
 
 # Test sonuçları ve tahminler
-test_acc = test_model[plan.get_acc()]
-test_preds = test_model[plan.get_preds()]
+test_acc = test_model[model_operations.get_acc()]
+test_preds = test_model[model_operations.get_preds()]
 
 # Modeli kaydetme
 model_operations.save_model(model_name='IMDB', scaler_params=scaler_params, W=W, activation_potentiation=activation_potentiation)
