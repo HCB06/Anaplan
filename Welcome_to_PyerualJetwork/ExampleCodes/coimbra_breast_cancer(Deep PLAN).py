79ca9501d2e5166164029fe233935e96fb0bbd62
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2025-01-24 05:58:44 +0300

    Update coimbra_breast_cancer(Deep PLAN).py

--- Welcome_to_PyerualJetwork/ExampleCodes/coimbra_breast_cancer(Deep PLAN).py
+++ Welcome_to_PyerualJetwork/ExampleCodes/coimbra_breast_cancer(Deep PLAN).py
@@ -27,10 +27,10 @@ x_train, y_train = data_operations.auto_balancer(x_train, y_train)
 scaler_params, x_train, x_test = data_operations.standard_scaler(x_train, x_test)
 
 # Configuring optimizator
-genetic_optimizator = lambda *args, **kwargs: planeat.evolve(*args, **kwargs)
+genetic_optimizer = lambda *args, **kwargs: planeat.evolve(*args, **kwargs)
 
 # Training Process
-model = plan.learner(x_train, y_train, genetic_optimizator, x_test, y_test, target_acc=0.82)
+model = plan.learner(x_train, y_train, genetic_optimizer, x_test, y_test, target_acc=0.82)
 
 activation_potentiation = model[model_operations.get_act_pot()]
 W = model[model_operations.get_weights()]
