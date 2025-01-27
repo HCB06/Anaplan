06a1577b8c2fd716516061177219465cccca25ad
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2025-01-27 20:37:46 +0300

    Update health-care-stroke(all algorithms).py

--- Welcome_to_PyerualJetwork/ExampleCodes/health-care-stroke(all algorithms).py
+++ Welcome_to_PyerualJetwork/ExampleCodes/health-care-stroke(all algorithms).py
@@ -7,7 +7,7 @@ Created on Thu Jun 20 03:55:15 2024
 
 import pandas as pd
 import numpy as np
-from pyerualjetwork import plan, data_operations, model_operations
+from pyerualjetwork import plan, planeat, data_operations, model_operations
 from colorama import Fore
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
