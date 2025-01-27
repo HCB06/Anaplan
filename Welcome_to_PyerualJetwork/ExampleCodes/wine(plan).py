6e7bad8475ea96afafa3fa7d72799678e6bee4bd
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2025-01-27 20:38:47 +0300

    Update wine(plan).py

--- Welcome_to_PyerualJetwork/ExampleCodes/wine(plan).py
+++ Welcome_to_PyerualJetwork/ExampleCodes/wine(plan).py
@@ -24,7 +24,7 @@ x_test, y_test = data_operations.auto_balancer(x_test, y_test)
 scaler_params, x_train, x_test = data_operations.standard_scaler(x_train, x_test)
 
 # Configuring optimizer
-genetic_optimizer = lambda *args, **kwargs: planeat.evolve(*args, **kwargs)
+genetic_optimizer = lambda *args, **kwargs: planeat.evolver(*args, **kwargs)
 model = plan.learner(x_train, y_train, genetic_optimizer, x_test, y_test, show_history=True, depth=2) # learner function = TFL(Test Feedback Learning). If test parameters not given then uses Train Feedback. More information: https://github.com/HCB06/pyerualjetwork/blob/main/Welcome_to_PLAN/PLAN.pdf
 
 W = model[model_operations.get_weights()]
