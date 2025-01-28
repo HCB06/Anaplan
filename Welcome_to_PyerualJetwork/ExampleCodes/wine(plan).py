4c2ae840a4e5f1eb1f3cada2bbbb5ec88c9c6cf9
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2025-01-29 01:43:27 +0300

    Update wine(plan).py

--- Welcome_to_PyerualJetwork/ExampleCodes/wine(plan).py
+++ Welcome_to_PyerualJetwork/ExampleCodes/wine(plan).py
@@ -5,11 +5,8 @@ Created on Tue Jun 18 23:32:16 2024
 @author: hasan
 """
 
-import time
-from colorama import Fore
 from pyerualjetwork import plan, planeat, data_operations, model_operations, metrics
 from sklearn.datasets import load_wine
-import numpy as np
 
 data = load_wine()
 X = data.data
@@ -25,7 +22,7 @@ scaler_params, x_train, x_test = data_operations.standard_scaler(x_train, x_test
 
 # Configuring optimizer
 genetic_optimizer = lambda *args, **kwargs: planeat.evolver(*args, **kwargs)
-model = plan.learner(x_train, y_train, genetic_optimizer, x_test, y_test, show_history=True, depth=2) # learner function = TFL(Test Feedback Learning). If test parameters not given then uses Train Feedback. More information: https://github.com/HCB06/pyerualjetwork/blob/main/Welcome_to_PLAN/PLAN.pdf
+model = plan.learner(x_train, y_train, genetic_optimizer, fit_start=True, show_history=True, gen=2)
 
 W = model[model_operations.get_weights()]
 activation_potentiation = model[model_operations.get_act_pot()]
@@ -40,5 +37,5 @@ model_path = ''
 
 model_operations.save_model(model_name=model_name, activation_potentiation=model[model_operations.get_act_pot()], model_path=model_path, scaler_params=scaler_params, W=model[model_operations.get_weights()])
 
-precisison, recall, f1 = metrics(y_test, test_preds)
+precisison, recall, f1 = metrics.metrics(y_test, test_preds)
 print('Precision: ', precisison, '\n', 'Recall: ', recall, '\n', 'F1: ', f1)
