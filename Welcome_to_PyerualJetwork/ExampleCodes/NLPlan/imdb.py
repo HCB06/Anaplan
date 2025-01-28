151070faccc0eca2560f3f88606718fb1236153a
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2025-01-29 00:19:50 +0300

    Update imdb.py

--- Welcome_to_PyerualJetwork/ExampleCodes/NLPlan/imdb.py
+++ Welcome_to_PyerualJetwork/ExampleCodes/NLPlan/imdb.py
@@ -43,7 +43,7 @@ scaler_params, x_train, x_test = data_operations.standard_scaler(x_train, x_test
 
 # Configuring optimizer
 genetic_optimizer = lambda *args, **kwargs: planeat.evolver(*args, **kwargs)
-model = plan.learner(x_train, y_train, genetic_optimizer, x_test, y_test, target_acc=0.85, auto_normalization=False, except_this=['circular']) # learner function = TFL(Test Feedback Learning). If test parameters not given then uses Train Feedback. More information: https://github.com/HCB06/pyerualjetwork/blob/main/Welcome_to_PLAN/PLAN.pdf
+model = plan.learner(x_train, y_train, genetic_optimizer, fit_start=True, gen=10, auto_normalization=False) # learner function = TFL(Test Feedback Learning). If test parameters not given then uses Train Feedback. More information: https://github.com/HCB06/pyerualjetwork/blob/main/Welcome_to_PLAN/PLAN.pdf
 
 W = model[model_operations.get_weights()]
 activation_potentiation = model[model_operations.get_act_pot()]
