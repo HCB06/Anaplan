ad544501e346285955f682540913c092dd43f7a3
Hasan Can Beydili <128186859+HCB06@users.noreply.github.com>
2025-01-30 20:58:36 +0300

    Update coimbra_breast_cancer(Deep PLAN).py

--- Welcome_to_PyerualJetwork/ExampleCodes/coimbra_breast_cancer(Deep PLAN).py
+++ Welcome_to_PyerualJetwork/ExampleCodes/coimbra_breast_cancer(Deep PLAN).py
@@ -27,18 +27,25 @@ x_train, y_train = data_operations.auto_balancer(x_train, y_train)
 scaler_params, x_train, x_test = data_operations.standard_scaler(x_train, x_test)
 
 # Configuring optimizator
-genetic_optimizer = lambda *args, **kwargs: planeat.evolver(*args, policy='aggressive', **kwargs)
+genetic_optimizer = lambda *args, **kwargs: planeat.evolver(*args, policy='aggressive', activation_selection_add_prob=0, activation_mutate_add_prob=0, **kwargs)
 
 # Training Process
-model = plan.learner(x_train, y_train, genetic_optimizer, fit_start=False, gen=50)
+model = plan.learner(x_train, y_train, genetic_optimizer, fit_start=True, gen=60)
 
 activation_potentiation = model[model_operations.get_act_pot()]
 W = model[model_operations.get_weights()]
 
 # Modeli test etme
-test_model = plan.evaluate(x_test, y_test, show_metrics=True,  W=W, activation_potentiation=activation_potentiation)
+test_model = plan.evaluate(x_test, y_test,  W=W, activation_potentiation=activation_potentiation)
+train_model = plan.evaluate(x_train, y_train, W=W, activation_potentiation=activation_potentiation)
+
 print(classification_report(plan.decode_one_hot(y_test), test_model[model_operations.get_preds()]))
 test_acc = test_model[model_operations.get_acc()]
+train_acc = train_model[model_operations.get_acc()]
+
+print(Fore.GREEN + "------PLAN Modeli Sonuçları------" + Fore.RESET)
+print(f"PLAN Test Accuracy: {test_acc:.4f}")
+print(f"PLAN Train Accuracy: {train_acc:.4f}")
 
 model_operations.save_model(model_name='breast_cancer_coimbra',
                 scaler_params=scaler_params,
